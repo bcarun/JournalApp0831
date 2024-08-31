0f0b01401b85b7e7a318eb1006e7804b8a2c91a5
Arun Chandrasekaran <bcarunmail@gmail.com>
2024-08-31 08:43:46 -0400

    Initial Commit

---
+++ JournalApp0831/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JournalApp0831
+//
+//  Created by Arun Chandrasekaran on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JournalApp0831/JournalApp0831App.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp0831App.swift
+//  JournalApp0831
+//
+//  Created by Arun Chandrasekaran on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp0831App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
